386ff39a892b47747ccd76c6bac6824da9ec0c4f
Bitwit Technologies <77882346+bitwittech@users.noreply.github.com>
2026-04-01 18:33:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const path = require("path");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server running 🎉");
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
